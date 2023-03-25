0ba070575600188434565dde05e8096d065b64f0
schalleneider <willians.schneider@gmail.com>
2023-03-25 17:00:30 -0400

    initial commit

---
+++ src/sqlite-to-parquet/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace sqlite_to_parquet
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            // Setup Host
+            var host = CreateDefaultBuilder().Build();
+
+            // Invoke Worker
+            using IServiceScope serviceScope = host.Services.CreateScope();
+            IServiceProvider provider = serviceScope.ServiceProvider;
+            var workerInstance = provider.GetRequiredService<Worker>();
+            await workerInstance.DoWorkAsync();
+
+            host.Run();
+        }
+
+        static IHostBuilder CreateDefaultBuilder()
+        {
+            return Host.CreateDefaultBuilder()
+                .ConfigureAppConfiguration(app =>
+                {
+                    app.AddJsonFile("appsettings.json");
+                })
+                .ConfigureServices(services =>
+                {
+                    services.AddSingleton<Worker>();
+                });
+        }
+    }
+}
---
+++ src/sqlite-to-parquet/Worker.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.Data.Sqlite;
+using Microsoft.Extensions.Configuration;
+using Parquet;
+using Parquet.Rows;
+using Parquet.Schema;
+
+namespace sqlite_to_parquet
+{
+    internal class Worker
+    {
+        private readonly IConfiguration configuration;
+
+        public Worker(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+
+        public async Task DoWorkAsync()
+        {
+            var databasePath = configuration.GetSection("database").GetValue<string>("path");
+            var databaseQuery = configuration.GetSection("database").GetValue<string>("query");
+            
+            var table = new Table(
+                new DataField<int>("Id"),
+                new DataField<string>("Name"),
+                new DataField<string>("Value"),
+                new DataField<bool>("IsEnabled")
+            );
+            
+            using (var connection = new SqliteConnection($"Data Source={databasePath}"))
+            {
+                connection.Open();
+
+                var command = connection.CreateCommand();
+                
+                command.CommandText = databaseQuery;
+
+                using (var reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        table.Add(
+                            reader.GetInt32(0),
+                            reader.GetString(1),
+                            reader.GetString(2),
+                            reader.GetBoolean(3)
+                        );
+                    }
+                }
+            }
+
+            await table.WriteAsync("output.parquet");
+        }
+    }
+}
